948c02585ba69ec0f259eb21639caa81c8b27c20
Brent Shierk <bshierk1996@gmail.com>
2023-07-21 01:35:45 -0400

    day 3/trivia &deploy to heroku

--- main.py
+++ main.py
@@ -4,7 +4,7 @@ from discord.voice_client import VoiceClient
 from discord.utils import get
 import requests
 import json
-
+import asyncio
 import time
 import asyncio
 
@@ -13,97 +13,53 @@ import os
 from os.path import join, dirname
 from dotenv import load_dotenv
 load_dotenv()
-OPEN_AI_API_KEY = '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'
+OPENAI_API_KEY = os.getenv('OPENAI_API_KEY')
 DISCORD_API_KEY =os.getenv('DISCORD_API_KEY')
 WEATHER_API_KEY =os.getenv('WEATHER_API_KEY')
 command_prefix = '!'
-client = discord.Client()
-bot = Bot(command_prefix=command_prefix)
-messages = joined = 0
-ROLE = 'new'
+intents = discord.Intents.default()
+intents.message_content = True
+client = discord.Client(intents=intents)
 
+def get_question():
+    qs = ''
+    id = 1
+    answer = 0
+    response = requests.get("http://127.0.0.1:8000/api/random/")
+    json_data = json.loads(response.text)
+    qs += "Question: \n"
+    qs += json_data[0]['title'] + '\n'
 
-@client.event
-async def on_ready():
-    print('We have logged in as {0.user}'.format(client))
+    for item in json_data[0]['answer']:
+        qs +=  str(id) + ". " + item['answer'] + "\n"
 
-@client.event
-async def member_join(member):
-    global joined
-    joined += 1
-    for channel in member.server.channgels:
-        if channel == "general":
-            await channel.send(f""" Welcome to the server {member.mention}""")
-    
-@client.event
-async def assign_role(member):
-    role = get(member.guild.roles, name=ROLE)
-    await member.add_roles(role)
-    print(f"{member} was given role {role}")
-
-
-
-@client.event
-async def on_member_update(before,after):
-    n = after.nick
-    if n:
-        if n.lower().count('brent') > 0:
-            last = before.nick
-            if last:
-                await after.edit(nick=last)
-            else:
-                await after.edit(nick='you cant be me')
-
-
-
+        if item['is_correct']:
+            answer = id
+        
+        id += 1
+    return(qs, answer)
 
 @client.event
 async def on_message(message):
-    id = client.get_guild(798095841671249921)
-    global messages 
-    messages += 1
-    channels = ['commands']
-    bad_words = ['peanut','almond','walnut']
-
-    for word in bad_words:
-        if message.content.count(word) > 0:
-            print('caught you talking about peanuts')
-            await message.channel.purge(limit=1)
-
-    if message.content == '!help':
-        embed = discord.Embed(title='channel commands',description='list of commands',)
-        embed.add_field(name='!hello',value='greets user')    
-        embed.add_field(name='!users',value='prints number of users')
-        await message.channel.send(content=None,embed=embed)
-    if str(message.channel) in channels:
-
-        if message.author == client.user:
-            return
-
-        if message.content.startswith('$hello'):
-            await message.channel.send('Hello!')
-        elif message.content == '!users':
-            await message.channel.send(f""" # of members: {id.member_count}""")
-
-        if message.author != client.user and message.content.startswith('w.'):
-            if len(message.content.replace('w.', '')) >= 1:
-                location = message.content.replace('w.', '').lower()
-                url = f'http://api.openweathermap.org/data/2.5/weather?q={location}&appid={WEATHER_API_KEY}&units=imperial'
-                try:
-                    data = parse_data(json.loads(requests.get(url).content)['main'])
-                    await message.channel.send(embed=weather_message(data, location))
-                    print(data)
-                except KeyError:
-                    await message.channel.send(embed=error_message(location))
+    if message.author == client.user:
+        return
+    if message.content.startswith('!q'):
+        q, ans = get_question()
+        await message.channel.send(q)
+    
+        def check(m):
+            return m.author == message.author and m.content.isdigit()
         
-        if message.content.startswith('!gpt'):
-            command=message.content.split(' ')[0]
-            user_message=message.content.replace('!gpt', '')
-            print(command,user_message)
-        if command == '!gpt':
-            bot_response = chatgpt_response(prompt=user_message)
-            await message.channel.send(f"Answer:{bot_response}")
+        try:
+            guess = await client.wait_for('message',check=check,timeout=10.0)
+        except asyncio.TimeoutError:
+            return await message.channel.send('Sorry, you did not answer quick enough')
+        
+        if int(guess.content) == ans:
+            await message.channel.send('Correct!')
+        else:
+            await message.channel.send('Sorry thats incorrect')
 
 
-client.loop.create_task(update_stats())
+
 client.run(DISCORD_API_KEY)
---
+++ utilbot/quiz/views.py
@@ -0,0 +1,11 @@
+from django.shortcuts import render
+from .models import Question
+from .serializers import RandomQuestionSerializer
+from  rest_framework.response import Response
+from rest_framework.views import APIView
+class RandomQuestion(APIView):
+
+    def get(self,request,formate=None, **kwargs):
+        question = Question.objects.filter().order_by('?')[:1]
+        serializer = RandomQuestionSerializer(question, many=True)
+        return Response(serializer.data)
